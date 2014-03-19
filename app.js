c44e9f4fab55c7370b94b7743bac970775d8bca4
kanonenfutter <vanakh@p215133.vpn-dyn.fh-koeln.de>
2014-03-19 17:30:58 +0100

    progress

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var url = require('url');
 
 var planeten =  [['Erde', '12800', '149600000'],
                 ['Mars', '6800', '227900000'],
@@ -11,12 +12,17 @@ var planeten =  [['Erde', '12800', '149600000'],
 
 
 
-http.createServer(function (req, res) {
+var server = http.createServer();
+    
+server.on('request', function (req, res){
+    
+    if(url.parse(req.url).pathname == '/planeten') {
+    
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<head><title>Planeten der Milchstrasse</title></head>');
     res.write('<body><h2>Unsere Planeten</h2><table><tr><th>Planetenname</th><th>Durchmesser</th><th>Entfernung zur Sonne</th></tr>');
               
-        for (var i = 0; i < 7; i++) {
+        for (var i = 0; i < planeten.length; i++) {
         res.write('<tr>');
         for (var j = 0; j < 3; j++) {
             res.write('<td>');
@@ -26,7 +32,20 @@ http.createServer(function (req, res) {
         }
         res.write('</tr>');
     }
+        res.write('<table>');
+    res.write('<form name="form" action="http://127.0.0.1:8124/planeten">Planetenname<input name="Pname" type="text"></input><br>Durchmesser<input name="Du" type="text"></input><br>Entfernung zur Sonne<input name="Dist" type="text"></input><br><input type="submit" formmethod="post" value="Absenden"></input></form>');
     res.write('</body>');
     res.end();
+    } 
+    else {    
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('Falscher Pfad');
+        res.end();}
+    req.on('post', function(){
+        planeten[0][0] = 'Neptun';
+        planeten[planeten.length][1] = '49244';
+        planeten[planeten.length][2] = '4503000000';
+    })
 }).listen(8124, "127.0.0.1");
 console.log('Server running at http://127.0.0.1:8124/');
+    
